90cb521f133269fbabd0e5eee411f4c43ffb7d21
Yertugan <ertugantoguzbaev@gmail.com>
2018-11-06 01:16:18 +0200

    new commit

---
+++ PageAnalyzer/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace PageAnalyzer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(396, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.textBox2.Location = new System.Drawing.Point(12, 107);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(566, 239);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(448, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 31);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Review";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label1.Location = new System.Drawing.Point(699, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 19);
+            this.label1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label2.Location = new System.Drawing.Point(699, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 19);
+            this.label2.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label3.Location = new System.Drawing.Point(699, 190);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 19);
+            this.label3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label4.Location = new System.Drawing.Point(699, 235);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 19);
+            this.label4.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label5.Location = new System.Drawing.Point(608, 107);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 19);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Actors:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label6.Location = new System.Drawing.Point(608, 147);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 19);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Plot:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label7.Location = new System.Drawing.Point(608, 190);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(61, 19);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Music:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label8.Location = new System.Drawing.Point(608, 235);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(65, 19);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Effects:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Lucida Calligraphy", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(651, 30);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(133, 36);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "Ratings";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(848, 390);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Movie Review";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ PageAnalyzer/Form1.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace PageAnalyzer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+        int actorScore;
+        int scenarioScore;
+        int musicScore;
+        string moviegood;
+        int effectsScore;
+        double stars;
+        string reviews;
+        string acting, effects, music, plot, plot2, music2, recomm;
+        List<string> reviewList = new List<string>();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        string MoviewTitle;
+        string goodas, badas, goodms, badms, goodefs, badefs, goodpls, badpls;
+
+        /// <summary>
+        /// Get all reviews of movie from imdb.com
+        /// </summary>
+        /// <param name="movie"></param>
+        /// <returns>reviews</returns>
+        protected string GetReview(string movie)
+        {
+            
+            
+            string UrlId = "https://www.imdb.com/find?ref_=nv_sr_fn&q=" + movie + "&s=all";
+            HtmlWeb webId = new HtmlWeb();
+            HtmlAgilityPack.HtmlDocument docId = webId.Load(UrlId);
+            // var node = doc.DocumentNode.SelectNodes("//div[@class='text show-more__control']");
+            var movieID = docId.DocumentNode.SelectSingleNode("//td[@class='result_text']");
+            string[] parts = movieID.InnerHtml.Split('/');
+            //textBox2.Text = parts[2];
+            string UrlReview = "http://www.imdb.com/title/" + parts[2] + "/reviews?ref_=tt_urv";
+            HtmlWeb web = new HtmlWeb();
+            HtmlAgilityPack.HtmlDocument docReview = webId.Load(UrlReview);
+            var movieReview = docReview.DocumentNode.SelectNodes("//div[@class='text show-more__control']");
+            string result = "";
+            foreach(var m in movieReview)
+            {
+                result += m.InnerText;
+                reviewList.Add(m.InnerText);
+            }
+            return result;
+        }
+        
+        /// <summary>
+        /// Button launch GetResult and GetReview methods and input results to labels and textbox2.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Refresh();
+            textBox2.Refresh();
+            label1.Refresh();
+            label2.Refresh();
+            label3.Refresh();
+            label4.Refresh();
+            reviews = GetReview(MoviewTitle);
+            
+            GetResult(reviews);
+            label1.Text = actorScore.ToString();
+            label2.Text = scenarioScore.ToString();
+            label3.Text = musicScore.ToString();
+            label4.Text = effectsScore.ToString();
+            string shortReview = "I want to give " + MoviewTitle + " " +  Math.Round(stars, 2) + " stars out of 10." + " First of all, an actor play was " + acting + 
+                ". There is no doubt that the scenario is " + plot + ", which made this movie " + plot2 +
+            ". The musical soundtrack was " + music + ". " + music2 + 
+            ". The last but not least is visual effects. In this movie visual effects were " + effects + 
+            ". All things considered, I would like to " + recomm +" to watch this " + moviegood + " movie.";
+            textBox2.Text = shortReview;
+        }
+
+        /// <summary>
+        /// Count stars, actore score, effects score, music score, plot score. 
+        /// </summary>
+        /// <param name="allReviews"></param>
+        public void GetResult(string allReviews)
+        {
+            int cnta = 1, cnts = 1, cntm = 1, cnte = 1;
+            int goodact = 0, badact = 0, goodp = 0, badp = 0, goodeff = 0, badeff = 0, goodmus = 0, badmus = 0;
+            actorScore = 1;
+            scenarioScore = 1;
+            musicScore = 1;
+            effectsScore = 1;
+            stars = 1;
+            string[] words = allReviews.Split(' ', '.', ',', '?', '!');
+            string[] actorKey = { "actor", "role", "actors", "artist", "performer", "player", "actioner" , "play"};
+            string[] scenarioKey = { "scenario", "plot", "plots", "storyline", "story", "action", "thread", "diegesis" };
+            string[] musicKey = { "melody", "singing", "sound", "soundtrack", "sound studio", "track", "song","musical", "songs", "singing", "sing", "music"};
+            string[] effectsKey = { "special effects", "visual effects", "effects", "effect", "animation", "camerawork", "computer graphics", "graphics" ,"visual", "graphic"};
+            string[] goods = { "good", "cool","superb","pretty","interesting","super","spectatcular", "excellent","fancy", "awesome", "great", "lovely", "love", "amazing", "perfect", "brilliant", "fantastic", "sparkling", "crowd-pleasing", "magnificent", "attractive","masterpiece","fresh", "beautiful" };
+            string[] bads = { "bad","not good", "awfull", "shit","ugly","crap","unworthy", "horrible", "cheep", "poor", "rough", "sad", "unacceptable", "lousy", "cheesy","pathetic","predictable", "fake", "dumb","dissapointing", "annoying", "sappy" };
+            Dictionary<string[], string[]> openWith = new Dictionary<string[], string[]>();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                for (int k = 0; k < goods.Length; k++)
+                {
+                    for (int l = 0; l < bads.Length; l++)
+                    {
+                        for (int j = 0; j < actorKey.Length; j++)
+                        {
+                            if (words[i] == actorKey[j])
+                            {
+                                if ((words[i + 1] == goods[k]) || (words[i - 1] == goods[k]) || (words[i + 2] == goods[k]) || words[i - 2] == goods[k])
+                                {
+
+                                    actorScore += 10;
+                                    goodact++;
+                                    cnta++;
+
+                                    //textBox2.Text += goods[k];
+                                }
+                                if ((words[i + 1] == bads[l]) || (words[i - 1] == bads[l]) || (words[i + 2] == bads[l]) || words[i - 2] == bads[l])
+                                {
+                                    actorScore += 1;
+                                    cnta++;
+                                    badact++;
+                                    //textBox2.Text += bads[l];
+                                }
+                            }
+                        }
+                        for (int j = 0; j < scenarioKey.Length; j++)
+                        {
+                            if (words[i] == scenarioKey[j])
+                            {
+                                if ((words[i + 1] == goods[k]) || (words[i - 1] == goods[k]) || (words[i + 2] == goods[k]) || words[i - 2] == goods[k])
+                                {
+
+                                    scenarioScore += 10;
+                                    goodp++;
+                                    cnts++;
+
+                                }
+                                if ((words[i + 1] == bads[l]) || (words[i - 1] == bads[l]) || (words[i + 2] == bads[l]) || words[i - 2] == bads[l])
+                                {
+                                    scenarioScore += 1;
+                                    cnts++;
+                                    badp++;
+                                }
+                            }
+                        }
+
+                        for (int j = 0; j < musicKey.Length; j++)
+                        {
+                            if (words[i] == musicKey[j])
+                            {
+                                if ((words[i + 1] == goods[k]) || (words[i - 1] == goods[k]) || (words[i + 2] == goods[k]) || words[i - 2] == goods[k])
+                                {
+
+                                    musicScore += 10;
+                                    goodmus++;
+                                    cntm++;
+
+                                }
+                                if ((words[i + 1] == bads[l]) || (words[i - 1] == bads[l]) || (words[i + 2] == bads[l]) || words[i - 2] == bads[l])
+                                {
+                                    musicScore += 1;
+                                    cntm++;
+                                    badmus++;
+                                }
+                            }
+                        }
+
+                        for (int j = 0; j < effectsKey.Length; j++)
+                        {
+                            if (words[i] == effectsKey[j])
+                            {
+                                if ((words[i + 1] == goods[k]) || (words[i - 1] == goods[k]) || (words[i + 2] == goods[k]) || words[i - 2] == goods[k])
+                                {
+
+                                    effectsScore += 10;
+                                    goodeff++;
+                                    cnte++;
+
+                                }
+                                if ((words[i + 1] == bads[l]) || (words[i - 1] == bads[l]) || (words[i + 2] == bads[l]) || words[i - 2] == bads[l])
+                                {
+                                    effectsScore += 1;
+                                    cnte++;
+                                    badeff++;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            actorScore = actorScore / cnta +1;
+            scenarioScore = scenarioScore / cnts +1;
+            musicScore = musicScore / cntm +1;
+            effectsScore = effectsScore / cnte +1;
+
+            stars = (actorScore + scenarioScore + musicScore + effectsScore) / 4 ;
+
+            int maincnt = 4;
+
+           
+            if (goodact > badact) acting = "great";
+            else if (badact > goodact) acting = "bad";
+            else acting = "not good not bad";
+
+            if (goodp > badp) { plot = "spectacular"; plot2 = "more interesting"; }
+            else if (badact > goodact) { plot = "poor"; plot2 = "very boring"; }
+            else { plot = "okay"; plot2 = "regular"; }
+
+            if (goodmus > badmus) { music = "amazing"; music2 = "I would like to add that soundtrack to my playlist"; }
+            else if (badmus > goodmus) { music = "not really good"; music2 = "I do not want to hear that music again"; }
+            else { music = "normal"; music2 = "I think I won't add that soundrack to my playlist"; }
+
+            if (goodeff > badeff) effects = "wonderful";
+            else if (badeff > goodeff) effects = "cheep";
+            else effects = "common";
+            if(cnta == 1)
+            {
+                actorScore = 0;
+                maincnt--;
+                stars = (actorScore + scenarioScore + musicScore + effectsScore) / maincnt;
+                acting = "not really memorable";
+                actorScore = 0;
+            }
+            if(cnte == 1)
+            {
+                effectsScore = 0;
+                maincnt--;
+                stars = (actorScore + scenarioScore + musicScore + effectsScore) / maincnt;
+                effects = "not much";
+                effectsScore = 0;
+            }
+
+            if(cntm == 1)
+            {
+                musicScore = 0;
+                maincnt--;
+                stars = (actorScore + scenarioScore + musicScore + effectsScore) / maincnt;
+                music = "not main part";
+               
+            }
+
+            if (stars > 5)
+            {
+                recomm = "recommend";
+                moviegood = "good";
+            }
+            else
+            {
+                moviegood = "bad";
+                recomm = "not recommend";
+            }
+        }
+        /// <summary>
+        /// User input movie title
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            MoviewTitle = textBox1.Text;
+        }
+    }
+}
